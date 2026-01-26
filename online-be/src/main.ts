fb3a38479386263fbb5d26610a031629b52131ba
M Defari RZ <m.defari.rz@gmail.com>
2026-01-26 13:25:32 +0700

    revert main.ts

--- online-be/src/main.ts
+++ online-be/src/main.ts
@@ -1,19 +1,5 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as crypto from 'crypto';
-
-if (!globalThis.crypto) {
-  globalThis.crypto = crypto as any;
-}
-
-if (!globalThis.crypto.randomUUID) {
-  globalThis.crypto.randomUUID = function () {
-    return crypto.randomUUID
-      ? crypto.randomUUID()
-      : require('crypto').randomBytes(16).toString('hex');
-  };
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
