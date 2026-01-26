48fd671bd7b855b2affd05f18bffdc05df21ec66
M Defari RZ <m.defari.rz@gmail.com>
2026-01-26 14:03:00 +0700

    delete preload

--- online-be/preload.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-globalThis.crypto = crypto;
-if (!globalThis.crypto.randomUUID) {
-  globalThis.crypto.randomUUID = function() {
-    return crypto.randomBytes(16).toString('hex');
-  };
-}
-console.log('✅ Crypto polyfill loaded');
