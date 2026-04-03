7e0217b74361842e2117accbe76716d94ffd852f
M Defari RZ <m.defari.rz@gmail.com>
2026-04-03 19:29:39 +0700

    add prefix api

--- online-be/src/main.ts
+++ online-be/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 8000;
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
